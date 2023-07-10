91bf849c346f1276990ef0829c475a8191c4bc18
CheoR <cheo.roman+github@gmail.com>
2023-07-10 10:58:16 -0500

    build: user can skip word

--- app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -103,7 +103,7 @@ fun GameScreen(
             }
 
             OutlinedButton(
-                onClick = { },
+                onClick = { gameViewModel.skipWord() },
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
